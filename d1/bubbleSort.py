69c73efee5b150f1e806a886fde65f1224541ea5
jeklen <zhql0907@outlook.com>
2017-09-25 19:02:57 +0800

    add bubblesort.py

---
+++ d1/bubbleSort.py
@@ -0,0 +1,15 @@
+def bubbleSort(List):
+    flag = True
+    while(flag):
+        j = 0
+        flag = False
+        while j < (len(List) -1):
+            if List[j] > List[j+1]:
+                List[j], List[j+1] = List[j+1], List[j]
+                j = j + 1
+                flag = True
+            else:
+                j = j + 1
+            print(List)
+    return List
+print(bubbleSort([99, 100, 3, 33, 9, 7, 10, 3]))
